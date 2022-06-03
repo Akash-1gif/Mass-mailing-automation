1d61d85a7d9e60241e89076e8e849e53189f9734
Akash <95571281+Akash-1gif@users.noreply.github.com>
2022-06-04 00:17:15 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,33 @@
+import smtplib
+import getpass
+from email.mime.text import MIMEText
+
+def fun1(l):
+    sender=input("enter sender's email address(use outlook mail)=")
+    password=getpass.getpass()
+    sub=input("enter subject:")
+    msg=input("enter message:\n")
+    print()
+    try:
+        server=smtplib.SMTP('smtp.outlook.com',587)
+        server.starttls()
+        server.login(sender,password)
+        msg=MIMEText(msg)
+        for i in l:
+            msg['Subject']=sub
+            msg['From']=sender
+            msg['To']=i
+            msg.set_param('importance','high value')
+            server.sendmail(sender,i,msg.as_string())
+            print(f"email to {i} has been sent")
+            print()
+        print("all mails have been sent")
+    except:
+        print("there is some problem in sending emails. Please try again")
+
+l=[]
+n=int(input("enter number of recipient="))
+for x in range(n):
+    y=input(f"enter email id of recipient {x+1}=")
+    l.append(y)
+fun1(l)
